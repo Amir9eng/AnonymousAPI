aa92c57019f3bcb6005b35475c8f1903c2330a8a
Amir9eng <amirmukhtarme@gmail.com>
2020-11-03 19:20:00 +0100

    morgan

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
 const app = express()
 const port = 3030
 
+app.use(cors())
+app.use(morgan('dev'))
+
 app.get('/', (req, res) => {
-  res.send('Anonymous Api')
+  res.send('Anonymous Api is live')
 })
 
 app.listen(port, () => {
